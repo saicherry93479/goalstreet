4769789bd97acbe862dfd0b718f002a356bd57a6
cherry <70464077+saicherry93479@users.noreply.github.com>
2024-03-31 22:20:03 +0530

    Update FAQNote.jsx

--- src/Components/FAQNote.jsx
+++ src/Components/FAQNote.jsx
@@ -10,13 +10,13 @@ const FAQNote = ({question,answer}) => {
     <div className="mb-2 w-full">
           <div className="border-b-[1.5px] border-black-500/10">
             <div className="flex items-center cursor-pointer" onClick={()=>setVisible(!visible)}>
-              <h3 className="relative flex flex-1 items-center justify-between transition-all [&[data-state=closed]>#plus>svg:first-child]:rotate-90 py-10 text-left font-bold hover:no-underline md:text-2xl text-black">
+              <h3 className="relative flex flex-1 items-center justify-between transition-all [&[data-state=closed]>#plus>svg:first-child]:rotate-90 py-4 md:py-10 text-left font-bold hover:no-underline md:text-2xl text-black">
                 {question}
 
               </h3>
               {visible?<Minus></Minus>:<PlusIcon></PlusIcon>}
             </div>
-            <div class={`pb-4 pt-0 text-black  ${visible?'':'h-0'} `}>
+            <div class={`pb-4 pt-0 text-black  ${visible?'':'hidden'} `}>
               {visible? answer:""}
             </div>
           </div>

02e642b743ead36c55aa614764f3d8d7252e518e
cherry <70464077+saicherry93479@users.noreply.github.com>
2024-04-15 22:43:56 +0530

    Create TermsPolicy.jsx

---
+++ src/Pages/TermsPolicy.jsx
@@ -0,0 +1,236 @@
+import React from "react";
+
+const TermsPolicy = () => {
+  return (
+    <div className="terms px-[20px] py-[20px] md:px-[60px] lg:px-[120px] bg-white  text-black font-bold min-h-[80vh]  bg-gray-100  sm:py-12">
+      <h2 >Terms & Conditions</h2>
+
+      <h2>1.Nature of Relationship</h2>
+      <p>
+        a) This MOU is for collaboration between both parties, for mutual
+        benefit, and for many purposes to enhance the quality of the educational
+        experience for students of the college.
+      </p>
+      <p>
+        b) This MOU shall be valid for Five years from the date of signing of
+        the MOU and each party shall be at full liberty to terminate the
+        collaboration with a notice period of 1 month.
+      </p>
+      <p>
+        c) Both parties shall take all reasonable steps to ensure the successful
+        completion of the collaboration and cooperate in duly carrying out the
+        obligation agreed upon.
+      </p>
+      <p>
+        d) The second party shall provide all the necessary facilities such as
+        infrastructure, Network, and Internet Access required for education and
+        training as training is conducted in the college.
+      </p>
+      <h2>2.Mutual obligation</h2>
+      <p>
+        a) This Collaboration shall not be exclusive to both parties and shall
+        not disallow each party from having similar collaboration with others.
+        Except as expressly stated in this MOU, there shall be no obligation on
+        any party to compensate the other in any manner to make any claim.
+      </p>
+      <p>
+        b) Each party shall respect the other's intellectual property (IP) and
+        shall not use any trade name, trademark, symbol, or designation
+        belonging to the other, without prior written approval. No party shall
+        hold out as an agent or representative of the other or create any
+        liability for the other.
+      </p>
+      <p>
+        c) Both parties shall maintain confidentiality about any information,
+        plans, discussions, strategies, or any material which shall be deemed to
+        be confidential and marked accordingly. d) Both the Parties should not
+        interact with faculties provided by each other except for the training
+        program and should not hire or offer any other financial proposals tothe
+        aforesaid during the period of the training program and for one year
+        after completion of the training.
+      </p>
+      <h2>3.Limitations and Warranties</h2>
+      <p>
+        a) Each party shall ensure that the other is not put to any liability
+        for any actions of the one party.
+      </p>
+      <p>
+        b) Each party represents that they have the full power and authority to
+        enter into this MOU in general.
+      </p>
+      <h2>4.General</h2>
+      <p>
+        a) Both parties will appoint a representative to act as the main
+        liaison, facilitating communication and coordination between them. These
+        designated individuals will serve as the primary points of contact,
+        streamlining collaboration and ensuring effective communication
+        throughout the partnership.
+      </p>
+      <p>
+        b) Goal Street will ensure the smooth execution and successful
+        administration of the internship program, providing valuable learning
+        opportunities for participants while upholding program objectives. Goal
+        Street will be responsible for the administration and execution of the
+        internship program.
+      </p>
+      <p>
+        c) Goal Street commits to strictly following the curriculum mutually
+        agreed upon by the company, which is documented and verified in the
+        ANNEXURE below.
+      </p>
+      <p>
+        d) Goal Street will leverage the internship program as an opportunity to
+        showcase and promote the participating companies' brands. Goal Street
+        will highlight the companies' contributions to the program, thereby
+        enhancing their visibility and reputation within relevant industries.
+        This branding initiative aims to mutually benefit both Goal Street and
+        the companies by fostering positive associations and attracting
+        potential talent and stakeholders.
+      </p>
+      <p>
+        e) Goal Street is committed to sourcing and nurturing top talent through
+        its internship program, ensuring that participating companies receive
+        skilled and capable individuals. This dedication to delivering
+        high-quality talent not only benefits the companies by meeting their
+        workforce needs but also strengthens Goal Street's reputation as a
+        provider of exceptional interns.
+      </p>
+      <p>
+        f) Capullo Private Limited will validate the internship program by
+        assessing its effectiveness in meeting predefined objectives and
+        criteria. By validating the internship program, Capullo Private Limited
+        ensures that it aligns with its standards and contributes positively to
+        its workforce development and talent acquisition strategies.
+      </p>
+      <p>
+        Capullo Private Limited will validate the course curriculum by
+        thoroughly reviewing its alignment with industry standards, relevance to
+        the company's objectives, and appropriateness for the intended audience.
+        Capullo Private Limited ensures that the course curriculum effectively
+        prepares participants with the knowledge and skills required to
+        contribute meaningfully to the company's operations and goals.
+      </p>
+      <p>
+        g) Capullo Private Limited will validate and verify the students
+        participating in the program through a comprehensive screening process.
+        Additionally, background checks or reference verifications may be
+        conducted to confirm the authenticity of the student's credentials and
+        experiences. By rigorously validating and verifying students, Capullo
+        Private Limited ensures that they meet the company's standards and
+        expectations, thereby maximizing the potential for a successful
+        internship experience and meaningful contributions to the company's
+        initiatives.
+      </p>
+      <p>
+        h) Capullo Private Limited will enhance the internship experience by
+        sharing real-time scenarios with participating students. These scenarios
+        offer practical insights into industry challenges, allowing students to
+        develop problem-solving skills and gain a deeper understanding of
+        workplace dynamics and decision-making processes.
+      </p>
+      <p>
+        i) Both parties shall not use the name of the other in any advertisement
+        or make any public announcement without the prior written approval of
+        the other.
+      </p>
+      <p>
+        j) Any dispute under this MOU will be settled in Hyderabad through
+        arbitration, if necessary in witness where both the parties have set
+        their hands to this MOU on the 3rd April 2024 at Hyderabad.
+      </p>
+      <h2>5.What is there in Internship?</h2>
+      <p>During an internship, participants can expect to:</p>
+      <p>
+        a) Gain Hands-On Experience: Interns have the opportunity to apply
+        theoretical knowledge from their academic studies to real-world tasks
+        and projects within a professional setting.
+      </p>
+      <p>
+        b) Learn from Professionals: Interns work alongside experienced
+        professionals who provide mentorship, guidance, and feedback, helping
+        them to develop industry-specific skills and insights.
+      </p>
+      <p>
+        {" "}
+        c) Network: Internships provide opportunities to connect with
+        professionals in the field, build relationships, and expand one's
+        professional network, which can be valuable for future career prospects.
+      </p>
+      <p>
+        d) Develop Skills: Interns have the chance to develop a wide range of
+        skills, including communication, teamwork, problem-solving, time
+        management, and technical proficiency, through practical application and
+        hands-on learning.
+      </p>
+
+      <p>
+        e) Explore Career Paths: Internships offer exposure to different roles
+        and responsibilities within an industry, helping participants to clarify
+        their career interests and goals.
+      </p>
+      <p>
+        f) Enhance Resume: Completing an internship adds valuable experience to
+        one'sresume, demonstrating to future employers a commitment to
+        professional development and readiness for the workforce.
+      </p>
+
+      <p>
+        g) Receive Feedback and Evaluation: Interns often receive feedback and
+        evaluations from supervisors or mentors, providing opportunities for
+        self-reflection and continuous improvement.
+      </p>
+      <h2>6.PURPOSE/SCOPE OF THE COLLABORATION:</h2>
+      <p>
+        {" "}
+        a) Access to Talent: Companies can identify and recruit top talent early
+        by offering internships. This allows them to assess potential future
+        employees and develop a pipeline of skilled candidates.
+      </p>
+      <p>
+        b) Fresh Perspectives: Interns often bring fresh ideas, perspectives,
+        and knowledge from their academic studies, which can contribute to
+        innovation and creativity within the company.
+      </p>
+      <p>
+        c) Increased Productivity: Interns can assist with tasks, projects, and
+        initiatives, helping to increase productivity and lighten the workload
+        for existing employees.
+      </p>
+
+      <p>
+        d) Cost-Effective Solutions: Interns are typically paid at a lower rate
+        than full-time employees, providing a cost-effective solution for
+        completing projects or tasks that may not require a full time employee.
+      </p>
+      <p>
+        e) Brand Building: Offering internships can enhance the company's
+        reputation as an employer of choice and attract top talent. Positive
+        internship experiences can lead to word-of-mouth referrals and positive
+        reviews, further enhancing the company's brand image.
+      </p>
+      <p>
+        f) Future Recruitment: Internships serve as a recruitment tool, allowing
+        companies to identify and nurture talent that may be suitable for future
+        full-time employment opportunities within the organization.
+      </p>
+      <p>
+        g) Diversity and Inclusion: Internship programs can help companies
+        promote diversity and inclusion by providing opportunities for students
+        from underrepresented backgrounds to gain valuable work experience and
+        access to career opportunities. NOTE: THE TRAINING CURRICULUM IS
+        ATTACHED AS AN ANNEXURE
+      </p>
+      <h2>7.Termination of Contract:</h2>
+      <p>
+        a) This contract will be terminated based on the consensus of both
+        parties.
+      </p>
+      <p>
+        b) If the contract is terminated, payment due for hours worked must be
+        paid to the First Party i.e, Goal Street.
+      </p>
+    </div>
+  );
+};
+
+export default TermsPolicy;
